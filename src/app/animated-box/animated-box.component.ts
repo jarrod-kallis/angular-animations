cefdcdff0563eea2b88a1886db6943a67f063795
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-08-12 17:19:17 +0200

    Routes & Animated Box Component

---
+++ src/app/animated-box/animated-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-box',
+  templateUrl: './animated-box.component.html',
+  styleUrls: ['./animated-box.component.css']
+})
+export class AnimatedBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { AdvancedComponent } from './routes/advanced/advanced.component';
+import { BasicComponent } from './routes/basic/basic.component';
+import { ContainedComponent } from './routes/contained/contained.component';
+import { AnimatedBoxComponent } from './animated-box/animated-box.component';
+
+const animationRoutes: Routes = [
+  {
+    path: 'animation-home',
+    component: HomeComponent
+  },
+  {
+    path: 'animation-basics',
+    component: BasicComponent
+  },
+  {
+    path: 'animation-contained',
+    component: ContainedComponent
+  },
+  {
+    path: 'animation-advanced',
+    component: AdvancedComponent
+  },
+  {
+    path: '',
+    redirectTo: '/animation-home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdvancedComponent,
+    BasicComponent,
+    ContainedComponent,
+    AnimatedBoxComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(animationRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
