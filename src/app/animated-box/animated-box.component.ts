492e0f019cae92f47976e77a62889ea6a24d9649
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-08-12 19:49:13 +0200

    Animation delay & easing

--- src/app/animated-box/animated-box.component.ts
+++ src/app/animated-box/animated-box.component.ts
@@ -13,10 +13,20 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
       })),
       state('basic', style({
         backgroundColor: '#440000',
-        transform: 'scale(2)'
+        transform: 'scale(1.7)'
+      })),
+      state('delaying', style({
+        backgroundColor: '#812170',
+        transform: 'scale(1.6)'
+      })),
+      state('easing', style({
+        backgroundColor: '#985b00',
+        transform: 'scale(1.8)'
       })),
       transition('* => basic', animate('800ms')),
-      transition('* => original', animate('200ms'))
+      transition('* => original', animate('200ms')),
+      transition('* => delaying', animate('800ms 1200ms ease-out')), // transition time, delay time, transition type
+      transition('* => easing', animate('800ms ease-in-out')),
     ])
   ]
 })
