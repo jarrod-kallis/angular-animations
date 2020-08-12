3b764655fa42990f19c02dee06aa26e1607e07ce
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-08-12 20:15:27 +0200

    Animations running in parallel

--- src/app/animated-box/animated-box.component.ts
+++ src/app/animated-box/animated-box.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
+import { trigger, state, style, animate, transition, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-animated-box',
@@ -27,6 +27,10 @@ import { trigger, state, style, animate, transition, keyframes } from '@angular/
         backgroundColor: 'black', // end state (after the animation has completed)
         transform: 'scale(1)'
       })),
+      state('parallel', style({
+        backgroundColor: 'yellow',
+        transform: 'scale(0.4)'
+      })),
       transition('* => basic', animate('800ms')),
       transition('* => original', animate('200ms')),
       transition('* => delaying', animate('800ms 1200ms ease-out')), // transition time, delay time, transition type
@@ -40,6 +44,16 @@ import { trigger, state, style, animate, transition, keyframes } from '@angular/
           style({ backgroundColor: 'red', transform: 'scale(1)', offset: 0.4 }), // then next 1s of 10s
           style({ backgroundColor: 'orange', transform: 'scale(1)', offset: 1 }) // then next 6s of 10s
         ]))
+      ]),
+      transition('* => parallel', [
+        group([
+          animate('5000ms ease-out', style({
+            backgroundColor: 'brown'
+          })),
+          animate('10000ms ease-out', style({
+            transform: 'scale(1.4)'
+          }))
+        ])
       ]),
     ])
   ]
